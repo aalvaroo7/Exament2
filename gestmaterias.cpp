b80c65acc734701112b874b610499bb84daefda5
aalvaroo7 <almarrom@myuax.com>
2023-12-14 12:24:42 +0100

    comit

--- gestmaterias.cpp
+++ gestmaterias.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <vector>
-#include <map>
 using namespace std;
+struct Estudiante {
+    int edad;
+    float promedio;
+    string nombre;
+};
+void mostrarestudiante(Estudiante est) {
+    cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << endl;
 
+    // Mostrar las materias
+    cout << "Materias inscritas: ";
+    for (const auto& materia : est.materias) {
+        cout << materia << " ";
+    }
+    cout << endl;
+}
+void agregarMateria(Estudiante& est, const string& nuevaMateria) {
+    est.materias.push_back(nuevaMateria);
+}
+int main(){
+    struct Estudiante estudiante1;
+    estudiante1.edad = 16;
+    estudiante1.promedio = 9.8;
+    estudiante1.nombre = "Juan"; // Asignando un valor al campo "nombre"
+}
