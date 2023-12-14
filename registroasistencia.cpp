c86da179bb408de64555493a8cf59e4db7e938e8
aalvaroo7 <almarrom@myuax.com>
2023-12-14 12:39:07 +0100

    comit

--- registroasistencia.cpp
+++ registroasistencia.cpp
@@ -10,3 +10,14 @@ struct Asistencia {
     string materia;
     string estado; // Puede ser "asistió", "falta" o "tardanza"
 };
+// Estructura Estudiante con un constructor
+struct Estudiante {
+    int id; // Identificador único para cada estudiante
+    int edad;
+    float promedio;
+    string nombre;
+    vector<Asistencia> asistencias; // Lista de asistencias del estudiante
+
+    // Constructor para inicializar la estructura
+    Estudiante(int id, int edad, float promedio, const string& nombre) : id(id), edad(edad), promedio(promedio), nombre(nombre) {}
+};
