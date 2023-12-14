63c6d21f7958af541a52fa7970d59032c84477fb
aalvaroo7 <almarrom@myuax.com>
2023-12-14 11:56:40 +0100

    comit

--- ej1.cpp
+++ ej1.cpp
@@ -3,9 +3,9 @@
 #include <string>
 using namespace std;
 struct Estudiante {
-char nombre [50];
-int edad;
-float promedio;
+    int edad;
+    float promedio;
+    string nombre;
 };
 void mostrarestudiante(Estudiante est){
 cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << endl;
