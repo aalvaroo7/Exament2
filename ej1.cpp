60990b024145582adcf06e996bbd40b79a5517d3
aalvaroo7 <almarrom@myuax.com>
2023-12-14 11:55:28 +0100

    comit

--- ej1.cpp
+++ ej1.cpp
@@ -10,3 +10,9 @@ float promedio;
 void mostrarestudiante(Estudiante est){
 cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << endl;
 }
+int main(){
+struct Estudiante estudiante1;
+    estudiante1.edad = 16;
+    estudiante1.promedio = 9.8;
+    estudiante1.nombre = "Juan"; // Asignando un valor al campo "nombre"
+}
