80a36bce1e35869abaadd3e1b46478bd6829f122
aalvaroo7 <almarrom@myuax.com>
2023-12-14 12:49:27 +0100

    comit

--- gestionfallos.cpp
+++ gestionfallos.cpp
@@ -12,3 +12,8 @@ class MateriaInvalidaException : public std::invalid_argument {
 public:
     MateriaInvalidaException(const std::string& mensaje) : std::invalid_argument(mensaje) {}
 };
+// Excepción personalizada para errores de estado
+class EstadoInvalidoException : public std::invalid_argument {
+public:
+    EstadoInvalidoException(const std::string& mensaje) : std::invalid_argument(mensaje) {}
+};
