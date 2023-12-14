e67ecbf85e6b95ea7caeff33bc5b66b1791c95c6
aalvaroo7 <almarrom@myuax.com>
2023-12-14 12:44:03 +0100

    comit

--- registroasist.c
+++ registroasist.c
@@ -8,3 +8,12 @@ struct Asistencia {
     char materia[50];
     char estado[20];  // Puede ser "asistió", "falta" o "tardanza"
 };
+// Definición de la estructura Estudiante
+struct Estudiante {
+    int id;      // Identificador único para cada estudiante
+    int edad;
+    float promedio;
+    char nombre[50];
+    struct Asistencia *asistencias;  // Lista de asistencias del estudiante
+    int numAsistencias;              // Número de asistencias registradas
+};
