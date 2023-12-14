63680c1393797f271173541701de5c25215bf5f6
aalvaroo7 <almarrom@myuax.com>
2023-12-14 12:28:34 +0100

    comit

--- gestmaterias.cpp
+++ gestmaterias.cpp
@@ -2,11 +2,15 @@
 #include <string>
 #include <vector>
 using namespace std;
+
+// Agrega el campo 'vector<string> materias' a la estructura Estudiante
 struct Estudiante {
     int edad;
     float promedio;
     string nombre;
+    vector<string> materias;  // Lista de materias en las que está inscrito
 };
+
 void mostrarestudiante(Estudiante est) {
     cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << endl;
 
@@ -17,24 +21,29 @@ void mostrarestudiante(Estudiante est) {
     }
     cout << endl;
 }
+
 void agregarMateria(Estudiante& est, const string& nuevaMateria) {
     est.materias.push_back(nuevaMateria);
 }
+
 void eliminarMateria(Estudiante& est, const string& materiaAEliminar) {
     // Utilizar la función erase y remove_if para eliminar la materia
     est.materias.erase(remove_if(est.materias.begin(), est.materias.end(),
                                  [&](const string& materia) { return materia == materiaAEliminar; }),
                        est.materias.end());
 }
-int main(){
+
+int main() {
     struct Estudiante estudiante1;
     estudiante1.edad = 16;
     estudiante1.promedio = 9.8;
     estudiante1.nombre = "Juan"; // Asignando un valor al campo "nombre"
+
     // Agregar materias
     agregarMateria(estudiante1, "Matemáticas");
     agregarMateria(estudiante1, "Física");
     agregarMateria(estudiante1, "Historia");
+
     // Mostrar información del estudiante
     mostrarestudiante(estudiante1);
 
