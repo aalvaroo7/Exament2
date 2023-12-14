3cebb2162c777517893160bcdce435d40cc274b4
aalvaroo7 <almarrom@myuax.com>
2023-12-14 12:43:44 +0100

    comit

--- registroasist.c
+++ registroasist.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+// Definición de la estructura Asistencia
+struct Asistencia {
+    char fecha[11];  // Considerando "YYYY-MM-DD" más el carácter nulo
+    char materia[50];
+    char estado[20];  // Puede ser "asistió", "falta" o "tardanza"
+};
