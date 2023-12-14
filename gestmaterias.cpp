61d3a83af2c0860ea66f1b91dca988898888bc69
aalvaroo7 <almarrom@myuax.com>
2023-12-14 12:36:22 +0100

    comit

--- gestmaterias.cpp
+++ gestmaterias.cpp
@@ -23,3 +23,33 @@ void eliminarMateria(Estudiante &e, const std::string &materia) {
         e.materias.erase(it);
     }
 }
+void mostrarMaterias(const Estudiante &e) {
+    std::cout << "Materias de " << e.nombre << ":" << std::endl;
+    for (const auto &materia : e.materias) {
+        std::cout << " - " << materia << std::endl;
+    }
+}
+// Ahora agregamos funcion donde se muestran los datos del estudiante
+
+void mostrarEstudiante(const Estudiante &e) {
+    std::cout << "Nombre: " << e.nombre << std::endl;
+    std::cout << "Edad: " << e.edad << std::endl;
+    std::cout << "Nota media: " << e.promedio << std::endl;
+    mostrarMaterias(e);
+}
+
+int main() {
+    // Volvemos a crear y a inicializar una instancia de Estudiante
+
+    Estudiante estudiante = {"Lydia Sanchez", 20, 8.5};
+
+    // Agregamos materias al estudiante
+
+    agregarMateria(estudiante, "Calculo I");
+    agregarMateria(estudiante, "Algebra Lineal");
+
+    // Y por último mostramos los datos del estudiante
+    mostrarEstudiante(estudiante);
+
+        return 0;
+}
