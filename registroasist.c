f2b56cff71572118f05fde9d51811c960b8abd8e
aalvaroo7 <almarrom@myuax.com>
2023-12-14 12:44:16 +0100

    comit

--- registroasist.c
+++ registroasist.c
@@ -17,3 +17,13 @@ struct Estudiante {
     struct Asistencia *asistencias;  // Lista de asistencias del estudiante
     int numAsistencias;              // Número de asistencias registradas
 };
+// Función para registrar la asistencia de un estudiante
+void registrarAsistencia(struct Estudiante *estudiante, const char *fecha, const char *materia, const char *estado) {
+    estudiante->asistencias = realloc(estudiante->asistencias, (estudiante->numAsistencias + 1) * sizeof(struct Asistencia));
+
+    strcpy(estudiante->asistencias[estudiante->numAsistencias].fecha, fecha);
+    strcpy(estudiante->asistencias[estudiante->numAsistencias].materia, materia);
+    strcpy(estudiante->asistencias[estudiante->numAsistencias].estado, estado);
+
+    estudiante->numAsistencias++;
+}
